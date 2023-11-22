32d917138493c8fb551f78283b3949962cf3dbd1
ekaterinazelenove <kate@clvlab.com>
2023-11-22 13:16:21 +0100

    add seo

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -2,28 +2,20 @@ import DifaultLayout from "@/layout/DefaultLayout"
 import client from "@/utils/apollo-client"
 import { gql } from "@apollo/client"
 import type { GetServerSidePropsContext } from "next"
-import Markdown from "react-markdown"
+import ReactMarkdown from "react-markdown"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { tomorrow } from "react-syntax-highlighter/dist/cjs/styles/prism"
 import { Heading, Box } from "@chakra-ui/react"
 import { useRef } from "react"
-
-const GET_POST_BY_ID_QUERY = gql`
-  query GetPostById($id: String!) {
-    blogPost(id: $id) {
-      title
-      content
-      mainImage {
-        url
-      }
-    }
-  }
-`
-const apolloClient = client()
+import { NextSeo } from "next-seo"
 
 type BlogPost = {
   title: string
   content: string
+  summary: string
+  sys: {
+    id: string
+  }
   mainImage: {
     url: string
   }
@@ -33,39 +25,67 @@ type PostProps = {
   blogPost: BlogPost
 }
 
+const GET_POST_BY_ID_QUERY = gql`
+  query GetPostById($id: String!) {
+    blogPost(id: $id) {
+      title
+      summary
+      content
+      sys {
+        id
+      }
+      mainImage {
+        url
+      }
+    }
+  }
+`
+const apolloClient = client()
+
 function PostPage(props: PostProps) {
   const {
-    blogPost: { content, title },
+    blogPost: { content, mainImage, summary, sys, title },
   } = props
 
   const highlighterRef = useRef<SyntaxHighlighter>(null)
 
   return (
     <DifaultLayout>
+      <NextSeo
+        title={`Ekaterina Zelenova | ${title}`}
+        description={summary}
+        openGraph={{
+          description: summary,
+          images: [
+            {
+              url: mainImage?.url,
+              alt: `Main Image for ${title}`,
+            },
+          ],
+          title: title,
+          type: "website",
+          url: `https://www.ekaterinazelenova.com/posts/${sys?.id}`,
+        }}
+      />
       <Heading as="h1">{title}</Heading>
-      <Box marginTop="8">
-        <Markdown
-          children={content}
+      <Box marginTop="8" whiteSpace="pre-wrap">
+        <ReactMarkdown
           components={{
-            code(props) {
-              const { children, className, node, ...rest } = props
+            code({ node, inline, className, children, ...props }) {
               const match = /language-(\w+)/.exec(className || "")
-              return match ? (
-                <SyntaxHighlighter
-                  ref={highlighterRef}
-                  PreTag="div"
-                  language={match[1]}
-                  style={tomorrow}>
+              return !inline && match ? (
+                <SyntaxHighlighter {...props} style={tomorrow} language={match[1]} PreTag="div">
                   {String(children).replace(/\n$/, "")}
                 </SyntaxHighlighter>
               ) : (
-                <code {...rest} className={className}>
+                <code {...props} className={className}>
                   {children}
                 </code>
               )
             },
-          }}
-        />
+          }}>
+          {content}
+        </ReactMarkdown>
       </Box>
     </DifaultLayout>
   )
